f17186f8e5727eb5d00f0972b8b0aea84ac186ba
hesterbergnl <nikolai.hesterberg@gmail.com>
2017-06-04 17:48:18 -0400

    Updated files on laptop version

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,36 @@
- 
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         // write testcode here
+        int [] original = {1,2,3,4};
+        int [] reverse = reverseCopy(original);
+        
+        System.out.println("Original: " + Arrays.toString(original));
+        System.out.println("Copied: "+ Arrays.toString(reverse));
+         
     }
+    
+    public static int[] reverseCopy(int[] array){
+        int i;
+        int[] revArray = new int[array.length];
+        for(i=array.length-1;i>=0;i--){
+            revArray[(array.length-1)-i] = array[i];
+        }
+        return revArray;
+    }
+    
+    public static int[] copy(int[] array){
+        int i;
+        int[] returnArray = new int[array.length];
+        
+        for(i=0;i<array.length;i++){
+            returnArray[i] = array[i];
+        }
+        
+        return returnArray;
+    }
+    
     
 }
--- week6-100.NightSky/src/Main.java
+++ week6-100.NightSky/src/Main.java
@@ -3,5 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your code here
+        NightSky test = new NightSky(0.8, 100, 500);
+        test.print();
+        System.out.println("Stars in last print: " + test.starsInLastPrint());
     }
 }
